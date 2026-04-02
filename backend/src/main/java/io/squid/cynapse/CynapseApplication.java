4a2e63dabe9d70903329e44340893f99f3c5ea37
TopeEstLa <contact.tope95@gmail.com>
2026-04-03 00:17:34 +0200

    add spring init backend

---
+++ backend/src/main/java/io/squid/cynapse/CynapseApplication.java
@@ -0,0 +1,13 @@
+package io.squid.cynapse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CynapseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CynapseApplication.class, args);
+	}
+
+}
