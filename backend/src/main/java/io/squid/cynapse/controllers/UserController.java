7b149a13479dcb0cbe3be724bda9cb9f69095822
TopeEstLa <contact.tope95@gmail.com>
2026-04-10 14:20:45 +0200

    Add /me endpoint

--- backend/src/main/java/io/squid/cynapse/controllers/UserController.java
+++ backend/src/main/java/io/squid/cynapse/controllers/UserController.java
@@ -5,6 +5,8 @@ import io.squid.cynapse.entities.User;
 import io.squid.cynapse.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +35,6 @@ public class UserController {
         for (User user : this.userRepository.findAll()) {
             userProfiles.add(new UserDTO.UserProfile(
                     user.getUsername(),
-                    5,
                     user.getMemberType(),
                     user.getBirthDate(),
                     user.getImage()
@@ -53,11 +54,30 @@ public class UserController {
 
         return ResponseEntity.ok(new UserDTO.UserProfile(
                 user.get().getUsername(),
-                5,
                 user.get().getMemberType(),
                 user.get().getBirthDate(),
                 user.get().getImage()
         ));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getMyProfile() {
+        Authentication  authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        if (currentUser == null) {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+
+        Optional<User> user = this.userRepository.findById(currentUser.getId());
+        if (user.isEmpty()) {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+
+
+        User userProfile = user.get();
+        userProfile.setPassword("");
+
+        return ResponseEntity.ok(userProfile);
     }
 
 }
