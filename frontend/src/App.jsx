87193ba51f8fc189b7bb0f3c9e9b4b3f95f8a549
Mathieu <mathieu.mt.nguyen@gmail.com>
2026-04-10 14:51:10 +0200

    Mathieu commit App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,83 +1,83 @@
-
+import './App.css';
 
 
 /* lancer le serveur en local : npm run dev a cette adresse http://localhost:5173*/
 
-function Register(){
-    return(
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <label>
-                  Pseudonyme :
-                  <input type="text" placeholder="Entrez votre pseudonyme" />
-                </label>
-              </tr>
-               <tr>
-                <label>
-                  Nom :
-                  <input type="text" placeholder="Entrez votre nom" />
-                </label>
-              </tr>
-               <tr>
-                <label>
-                  Prénom :
-                  <input type="text" placeholder="Entrez votre prénom" />
-                </label>
-              </tr>
-              <tr>
-                  Sexe :
-                  <input type="radio" id="H" name="ChoseSexe" value="Homme"/>
-                  <label for="H">Homme</label>
-                  <input type="radio" id="F" name="ChoseSexe" value="Femme"/>
-                  <label for="F">Femme</label>
-                  <input type="radio" id="O" name="ChoseSexe" value="Autre"/>
-                  <label for="O">Autre</label>
-              </tr>
-              <tr>
-                <label>
-                  Date de naissance :
-                  <input type="date" />
-                </label>
-              </tr>
-              <tr>
-                <label>
-                  Mot de passe :
-                  <input type="password" placeholder="MotDePasse"/>
-                </label>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-    );
+function Register() {
+  return (
+    <form>
+      <table>
+        <tbody>
+          <tr>
+            <label>
+              Pseudonyme :
+              <input type="text" placeholder="Entrez votre pseudonyme" />
+            </label>
+          </tr>
+          <tr>
+            <label>
+              Nom :
+              <input type="text" placeholder="Entrez votre nom" />
+            </label>
+          </tr>
+          <tr>
+            <label>
+              Prénom :
+              <input type="text" placeholder="Entrez votre prénom" />
+            </label>
+          </tr>
+          <tr>
+            Sexe :
+            <input type="radio" id="H" name="ChoseSexe" value="Homme" />
+            <label for="H">Homme</label>
+            <input type="radio" id="F" name="ChoseSexe" value="Femme" />
+            <label for="F">Femme</label>
+            <input type="radio" id="O" name="ChoseSexe" value="Autre" />
+            <label for="O">Autre</label>
+          </tr>
+          <tr>
+            <label>
+              Date de naissance :
+              <input type="date" />
+            </label>
+          </tr>
+          <tr>
+            <label>
+              Mot de passe :
+              <input type="password" placeholder="MotDePasse" />
+            </label>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
 }
 
-function Login(){
-    return(
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <label>
-                  Pseudonyme :
-                  <input type="text" placeholder="Entrez votre pseudonyme" id="pseudonym"/>
-                </label>
-              </tr>
-              <tr>
-                <label>
-                  Mot de passe :
-                  <input type="password" placeholder="MotDePasse" id="password"/>
-                </label>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-    );
+function Login() {
+  return (
+    <form>
+      <table>
+        <tbody>
+          <tr>
+            <label>
+              Pseudonyme :
+              <input type="text" placeholder="Entrez votre pseudonyme" id="pseudonym" />
+            </label>
+          </tr>
+          <tr>
+            <label>
+              Mot de passe :
+              <input type="password" placeholder="MotDePasse" id="password" />
+            </label>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
 }
 
-function ShowThermostat(){
-  
+function ShowThermostat() {
+
 }
 
 export default function MyApp() {
